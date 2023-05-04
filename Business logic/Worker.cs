1c2dc56998e5e8fa722c3f212ad31ab79c0f6db9
Amina S <aminchik2809@mail.ru>
2023-05-05 01:21:02 +0300

    add changes

--- Business logic/Worker.cs
+++ Business logic/Worker.cs
@@ -7,7 +7,6 @@ namespace graduationProject
 {
     public class Worker
     {
-        public int Id { get; set; }
         public long TelegramId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
